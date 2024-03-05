83edc23520186654fc65d8a7112537a043980b28
qzhq747 <qzhq747@gmail.com>
2024-03-05 21:12:14 +0800

    complete version调整速度

--- driver_cruise/driver_cruise.cpp
+++ driver_cruise/driver_cruise.cpp
@@ -55,7 +55,7 @@ static int InitFuncPt(int, void* pt)
 //Global variables for vehicle states
 static float _midline[200][2];
 static float _yaw, _yawrate, _speed, _acc, _width, _rpm;
-static int _gearbox;
+static int _gearbox; 
 
 static void userDriverGetParam(float midline[200][2], float yaw, float yawrate, float speed, float acc, float width, int gearbox, float rpm)
 {
@@ -194,6 +194,7 @@ public:
 	}
 };//高速下方向调整减小
 
+	
 
 /*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
 █	车辆控制主程序，由ChatGPT自动生成助教完善								█
@@ -206,56 +207,15 @@ bool isFirstFrame = true;		//第一帧标志
 
 static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, int* cmdGear)
 {
-	/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
-	█	舵角控制																█
-	\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
-	double targetAngleError = 0.0; //目标误差
-	double currentAngleError = atan2(_midline[5][0], _midline[5][1]); //当前误差
-	if (_speed > 150)//不同速度下误差∠需要不同设置
-		currentAngleError = atan2(_midline[20][0], _midline[20][1]);
-	else if(_speed > 100)
-		currentAngleError = atan2(_midline[10][0], _midline[10][1]);
-
-	//第一帧初始化舵角控制参数，清空积分器和微分器，因为控制目标为恒零，所以只需要初始化一次
-	if (isFirstFrame)
-	{
-		isFirstFrame = false;
-		angleController.initial(1.0, 0.0, 0.0, targetAngleError);
-	}
-	
-	//舵角PID控制
-	*cmdSteer = constrain(-1.0, 1.0, angleController.calculate(currentAngleError));
-
-
-	/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
-	█	速度控制																█
-	\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+	double cal_the_error = 0;
+	cal_the_error = sqrt(_midline[0][0] * _midline[0][0] + _midline[0][1] * _midline[0][1]);
+	//计算error值，在后文输出error值以考察算法性能
+#pragma region <<< cal_targetSpeed
 	double targetSpeed;  //目标车速
 	double currentSpeed = _speed;	//当前误差
-	
-	//计算前方是直道还是弯道
 	circle myCurve;
 	float minCruve = 500.0;
-	/*fist version
-	for (int fStep = 0; fStep < 50; fStep++)//可以调到180
-	{
-		myCurve = getR(_midline[fStep][0], _midline[fStep][1], _midline[fStep + 10][0], _midline[fStep + 10][1], _midline[fStep + 20][0], _midline[fStep + 20][1]);
-		if (myCurve.r < minCruve)
-		{
-			minCruve = myCurve.r;
-		}
-	//探索未来的赛道的最小曲率
-	}
-	//设定目标速度，如果前方弯道就设定低，直道就设定高
-	if (minCruve > 300)
-		targetSpeed = 250;
-	else if (minCruve > 150)
-		targetSpeed = 150;
-	else if (minCruve > 100)
-		targetSpeed = 80;
-	else
-		targetSpeed = 50;
-	*/
+	float minCruve_100 = 500;
 	for (int fStep = 0; fStep < 50; fStep++)//先检测前50米
 	{
 		myCurve = getR(_midline[fStep][0], _midline[fStep][1], _midline[fStep + 10][0], _midline[fStep + 10][1], _midline[fStep + 20][0], _midline[fStep + 20][1]);
@@ -264,105 +224,116 @@ static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, 
 			minCruve = myCurve.r;
 		}
 	}
+
+	minCruve_100 = minCruve;
 	for (int fStep = 50; fStep < 100; fStep++)//检测前100米，在速度稍大（100）的情况下，是否有需要急刹的情况
 	{
 		myCurve = getR(_midline[fStep][0], _midline[fStep][1], _midline[fStep + 10][0], _midline[fStep + 10][1], _midline[fStep + 20][0], _midline[fStep + 20][1]);
-	//探索未来的赛道的最小曲率
-		if (myCurve.r<= 100 && _speed > 100) {
-			minCruve = myCurve.r;
-			targetSpeed = 50;
-			break;
+		//探索未来的赛道的最小曲率
+		if (myCurve.r < minCruve_100) {
+			minCruve_100 = myCurve.r;
 		}
 	}
+	//设定目标速度，如果前方弯道就设定低，直道就设定高
+	if (minCruve > 480)
+		targetSpeed = 300;
+	else if (minCruve > 200)
+		targetSpeed = 90 + minCruve * 0.15 - 0.15 * 200;
+	else if (minCruve > 100)
+		targetSpeed = 65 + minCruve * 0.25 - 0.25 * 100;
+	else
+		targetSpeed = 35 + minCruve * 0.25;
+	if (minCruve_100 < 200 && minCruve_100 > 120 && targetSpeed > 100 && _speed > 100) {
+		targetSpeed = 100;
+	}//若探测到前方100米赛道有弯角且自车速度过快，提前刹车
+	else if (minCruve_100 <= 120 && targetSpeed > 75 && _speed > 100) {
+		targetSpeed = 75;
+	}//如上
+
 	for (int fStep = 100; fStep < 180; fStep++)//检测前200米，在速度较大（150）的情况下，是否有需要急刹的情况
 	{
 		myCurve = getR(_midline[fStep][0], _midline[fStep][1], _midline[fStep + 10][0], _midline[fStep + 10][1], _midline[fStep + 20][0], _midline[fStep + 20][1]);
-		//探索未来的赛道的最小曲率
 		if (myCurve.r <= 100 && _speed > 150) {
 			minCruve = myCurve.r;
-			targetSpeed = 50;
+			if (targetSpeed > 50)
+				targetSpeed = 50;
+			break;
+		}
+		if (myCurve.r <= 200 && _speed > 150) {
+			minCruve = myCurve.r;
+			if (targetSpeed > 80)
+				targetSpeed = 80;
 			break;
 		}
 	}
+#pragma endregion >>>
+#pragma region <<< cal_steer
+	double currentAngleError;//当前误差
+	double targetAngleError = 0;
+	currentAngleError = atan2((_midline[1][0]+ 2*_midline[0][0])/3, (_midline[1][1] + 2*_midline[0][1]) / 3);
+	//尤里卡！！！！！瞄点越近越好，没有近的，那就创造近的！
 
-	//设定目标速度，如果前方弯道就设定低，直道就设定高
-	if (minCruve > 300)
-		targetSpeed = 180;
-	else if (minCruve > 150)
-		targetSpeed = 150;
-	else if (minCruve > 100)
-		targetSpeed = 80;
-	else
-		targetSpeed = 50;
-
-	//每当目标速度变化时初始化PID控制器，重设参数，清空积分器和微分器
-	if (targetSpeed != lastTargetSpeed)
+	//第一帧初始化舵角控制参数，清空积分器和微分器，因为控制目标为恒零，所以只需要初始化一次
+	if (isFirstFrame)
 	{
-		speedController.initial(0.01, 0.0, 0.0, targetSpeed);
-		lastTargetSpeed = targetSpeed;
+		isFirstFrame = false;
+		//angleController.initial(3, 0.001, 4, targetAngleError);
+		angleController.initial(4, 0.001, 4, targetAngleError);
 	}
 
+	//舵角PID控制
+	*cmdSteer = constrain(-1.0, 1.0, angleController.calculate(currentAngleError));
+#pragma endregion >>>
 	//根据当前速度和目标速度关系，控制油门刹车以改变速度
 	if (currentSpeed < targetSpeed)  //当前速度低于目标，需要加速
 	{
-		
-		if (_speed > 100 && abs(*cmdSteer) < 0.1)
-		{
-			//速度较快且舵角较小时，使用PID控制
-			*cmdAcc = constrain(0.0, 1.0, speedController.calculate(currentSpeed));
-		}
-		else if (_speed > 50 && abs(*cmdSteer) < 0.1)
-		{
-			//速度较慢且舵角较小时，限定油门
-			*cmdAcc = 0.6;
-		}
+#pragma region <<< accelarate with tcs
+		if (abs(*cmdSteer) < 0.015 && targetSpeed==300&& cal_the_error<0.01) {
+			*cmdAcc = constrain(0, 1, 0.3 + _speed * 0.007);
+		}//在低速情况下，避免过大油门造成车辆的打滑，简单使用速度的线性关系计算油门开度
 		else
 		{
-			//除此之外的情况进一步限定油门
 			*cmdAcc = 0.3;
 		}
 		//加速情况下，刹车为0
 		*cmdBrake = 0;
+#pragma endregion >>>
 	}
 	else
-	{
-		//减速情况下，刹车
-		//增加循迹刹车功能
+	{	
+#pragma region <<< brake with abs
+	/*减速情况下，刹车
+	增加ABS防抱死刹车功能，不同速度下刹车力度不同以防止车轮抱死，
+	进而提高刹车性能，同时保持车辆转向能力*/
 		*cmdAcc = 0;
-		double abssteer= *cmdSteer;//转向绝对值
-		if (*cmdSteer < 0)
-			abssteer = -*cmdSteer;
-		if (abssteer > 0) //带转向情况下，防抱死刹车
-			if (_speed > 150)
-				*cmdBrake = 0.5;
-			else if (_speed > 100)
-				*cmdBrake = 0.4;
-			else
-				*cmdBrake = 0.3;
-		else {//不带转向情况下的防抱死刹车
-			double brake_control = _speed * 0.01;//刹车力度判断
-			if (brake_control > 1.5)
-				*cmdBrake = 1.0;
-			else if (brake_control > 1.4)
-				*cmdBrake = 0.9;
-			else if (brake_control > 1.3)
-				*cmdBrake = 0.8;
-			else if (brake_control > 1.2)
-				*cmdBrake = 0.7;
-			else if (brake_control > 1.1)
-				*cmdBrake = 0.6;
-			else if (brake_control > 1.0)
-				*cmdBrake = 0.5;
-
-			else
-				*cmdBrake = 0.3;
-		}
+		if(_speed > 170)
+			*cmdBrake=constrain( 0,1, 0.725+_speed*0.005-170 * 0.005);
+		else if(_speed>160)
+			*cmdBrake = 0.7;
+		else if (_speed > 150)
+			*cmdBrake = 0.675;
+		else if (_speed > 140)
+			*cmdBrake = 0.60 + _speed * 0.005 -140 * 0.005;
+		else if (_speed > 130)
+			*cmdBrake = 0.58+_speed*0.002-130*0.002;
+		else if (_speed > 120)
+			*cmdBrake = 0.55+_speed * 0.003 - 120 * 0.003;
+		else if (_speed > 110)
+			*cmdBrake = 0.55;
+		else if (_speed > 100)
+			*cmdBrake = 0.5;
+		else if (_speed > 90)
+			*cmdBrake = 0.45;
+		else if (_speed > 80)
+			*cmdBrake = 0.42;
+		else if (_speed > 70)
+			*cmdBrake = 0.38;
+		else
+			*cmdBrake = 0.3;
+#pragma endregion >>>
 	}
-	
 	//更新档位
 	updateGear(cmdGear);
-
-	//窗口可视化
-	 cls_visual.Fig2Y(1, -1, 1, 0, 500, 10, "Steer", *cmdSteer, "targetSpeed", targetSpeed,"*brake_control" , *cmdBrake);
-	//cls_visual.Fig2Y(1, 0, 300, 0, 500, 10, "Target V", targetSpeed, "Curvature", minCruve, "Current V", _speed);
+	//窗口可视化minCruve
+	 cls_visual.Fig2Y(1, 0, 500, 0, 1, 10, "targetSpeed", targetSpeed, "error", cal_the_error,"minCruve" , minCruve);
 }
